fc795c70de96bc2ff61bf3a7f1a59addfb4e2146
arturmelo <artur.melo@beubi.com>
2014-07-08 20:54:41 +0100

    Add install of php5-intl and locales configuration

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,21 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+#intl extension required in symfony 2 projects
+package "php5-intl" do
+    action :install
+end
+
+# This configures locale settings
+package "locales" do
+  action :install
+end
+
+execute "Update locale LANG" do
+  command "update-locale LANG=en_US.utf8"
+end
+
+execute "Update locale LC_ALL" do
+  command "update-locale LC_ALL=en_US.utf8"
+end
